f4628ed5a5f950a07fc6076fd0c9832fd3486a45
nikhil <nkmittal4994@gmail.com>
2015-08-08 20:30:38 +0530

    doc string for cost()

---
+++ travel.py
@@ -0,0 +1,32 @@
+def travelling():
+    """
+    """
+    pass
+
+
+def cost(set_Vertices, node):
+    """
+    cost of the minimum cost path visiting each vertex in set S exactly once,
+    starting at 0 and ending at node.
+
+    Paramters
+    ---------
+    set_Vertices : set
+        A set of vertices of graph.
+
+    node : int
+        The vertex to which we need minimum cost.
+    """
+    if len(set_Vertices) == 2:
+        pass
+    else:
+        pass
+    pass
+
+"""
+>>> s = set()
+>>> s |= set(list(range(24)))
+>>> s
+{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}
+>>> 
+"""
