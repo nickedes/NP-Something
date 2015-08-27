ff2b0d62b13d82d120fa66f3c1fd75909ef7eef9
prerit2010 <prerit2010@gmail.com>
2015-08-27 13:41:13 +0530

    generated initial sbox from Pwlcm,set initial conditions

--- travel.py
+++ travel.py
@@ -5,7 +5,7 @@ from test import nonlinearity
 from datetime import datetime
 import json
 
-
+global graph
 def getfilename():
     """
     Returns the current timestamp and will be used as filename. 
@@ -17,6 +17,7 @@ def getfilename():
 def travelling(array, num):
     """
     """
+    global graph
     graph = undirected(array, num)
     cost_path = []
     paths = {}
@@ -57,6 +58,7 @@ def dist(graph, i, j):
 def substitution(array, array_mod, num):
     """
     """
+
     cost_path, paths = travelling(array, num)
     for index in paths[min(cost_path)]:
         if array[8*num + index] not in array_mod:
@@ -85,6 +87,7 @@ def cost(graph, set_Vertices):
 
 if __name__ == '__main__':
     non_sbox = {}
+    graphs = []
     for var in range(32):
         print(var)
         array = gen()
@@ -94,22 +97,14 @@ if __name__ == '__main__':
             array_mod = []
             for num in range(32):
                 array_mod = substitution(array, array_mod, num)
-            with open('data2.txt', 'w') as f:
-                f.write(json.dumps(array_mod))
-            nn_array_mod = nonlinearity(array_mod)
-            for num in range(16):
-                if sum(nn_array)/8 > sum(nn_array_mod)/8:
-                    for index in range(len(nn_array_mod)):
-                        if nn_array_mod[index] < nn_array[index]:
-                            # print(nn_array[index], nn_array_mod[index])
-                            array_mod = substitution(array, array_mod, index)
-                    nn_array_mod = nonlinearity(array_mod)
-            print(sum(nn_array)/8, sum(nn_array_mod)/8)
-            if sum(nn_array)/8 < sum(nn_array_mod)/8:
-                if sum(nn_array_mod)/8 not in non_sbox:
-                    non_sbox[sum(nn_array_mod)/8] = array_mod
+                graphs.append(graph)
+            nn_array_mod = int(sum(nonlinearity(array_mod))/8)
+            non_sbox[nn_array_mod] = [array_mod,graphs]
+            print(sum(nn_array)/8, nn_array_mod)
         else:
             print('Is not bijective!')
-    with open('data/'+getfilename(), 'w') as f:
+    with open('data/'+getfilename(), 'a') as f:
+        f.write(json.dumps(array_mod))
         f.write(json.dumps(non_sbox))
     print(non_sbox.keys(), max(non_sbox.keys()))
+
